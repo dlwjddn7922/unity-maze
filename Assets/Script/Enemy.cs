320c560bd779e685a8a21aa5cd84645f01da2219
dlwjddn7922 <79899654+dlwjddn7922@users.noreply.github.com>
2023-06-13 19:55:27 +0900

    dd
    충돌

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * 5f);
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        transform.position += (new Vector3(x, y, 0f) * Time.deltaTime * 5);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("OnTrigger[Stay]2D");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("OnTrigger[Exit]2D");
+    }
+}
